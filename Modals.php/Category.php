77f7a32ceec35e12f45d2ef72ce49dc5bf2f1701
macchiagabriele13 <macchia_gabriele@libero.it>
2022-12-12 17:34:43 +0100

    Commit - Stampa card

--- Modals.php/Category.php
+++ Modals.php/Category.php
@@ -4,11 +4,21 @@
 
 class Category
 {
-    public $type;
-    public $icon;
+    public $category;
+    private $icon;
 
-    public function __construct(String $type)
+    public function __construct(String $category)
     {
-        $this->type = $type;
+        $this->category = $category;
+        $this->set_icon($category);
+    }
+
+    private function set_icon($category)
+    {
+        if ($category === 'Gatto') {
+            $this->icon = 'https://www.bing.com/images/search?view=detailV2&ccid=dwKf0Gc%2b&id=45342D66353FE4AA97DCA6140CA63E8939270BF0&thid=OIP.dwKf0Gc-eX9XyPcSEfp-HwHaH6&mediaurl=https%3a%2f%2fthumbs.dreamstime.com%2fz%2ficona-semplice-di-vettore-del-gatto-illustrazione-bianco-e-nero-del-gatto-icona-lineare-della-testa-del-gatto-del-profilo-93723103.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.77029fd0673e797f57c8f71211fa7e1f%3frik%3d8AsnOYk%252bpgwUpg%26pid%3dImgRaw%26r%3d0&exph=1390&expw=1300&q=iconagatto&simid=608001738536013145&FORM=IRPRST&ck=C9E5A04DB786A4E1E466046DAF66EF15&selectedIndex=5&ajaxhist=0&ajaxserp=0';
+        } else if ($category === 'Cane') {
+            $this->icon = 'https://www.bing.com/images/search?view=detailV2&ccid=fwuYy78L&id=41B767731B7EA9779B6B0558AE88852965581F8A&thid=OIP.fwuYy78LxkFQcfye9z6pYwHaHa&mediaurl=https%3a%2f%2fstatic.vecteezy.com%2fsystem%2fresources%2fpreviews%2f000%2f567%2f006%2foriginal%2fvector-dog-icon.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.7f0b98cbbf0bc6415071fc9ef73ea963%3frik%3dih9YZSmFiK5YBQ%26pid%3dImgRaw%26r%3d0&exph=5000&expw=5000&q=icona+cane+&simid=608009057161651011&FORM=IRPRST&ck=4D502D57C5B537CF231B5B998A029BD9&selectedIndex=18&ajaxhist=0&ajaxserp=0';
+        }
     }
 }
--- index.php
+++ index.php
@@ -19,6 +19,69 @@ require __DIR__ . "/Modals.php/Category.php";
 require __DIR__ . "/Modals.php/Product.php";
 require __DIR__ . "/Modals.php/Type.php";
 
+$products = [
+    $item, $item_due, $item_tre
+];
 
-$product = new Product('baubau', 'http', 20, new Category('Cane'), new Type('Cibo'));
-var_dump($product);
+
+$item = new Product('baubau', 'https://www.monge.it/wp-content/uploads/2022/06/ADULT-TACCHINO-25-319x500.jpg', 20, new Category('Cane'), new Type('Cibo'));
+var_dump($item);
+
+$item_due = new Product('baubau', 'https://www.monge.it/wp-content/uploads/2022/06/ADULT-TACCHINO-25-319x500.jpg', 20, new Category('Cane'), new Type('Cibo'));
+var_dump($item_due);
+
+$item_tre = new Product('baubau', 'https://www.monge.it/wp-content/uploads/2022/06/ADULT-TACCHINO-25-319x500.jpg', 20, new Category('Cane'), new Type('Cibo'));
+var_dump($item_tre);
+
+
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <?php foreach ($products as $product) : ?>
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="<?= $product->image ?>" alt="">
+                            <div class="card-body">
+                                <h4 class="card-title"><?= $product->name ?></h4>
+                                <p class="card-text"><?= $product->price ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+            </div>
+        </div>
+        </div>
+    </section>
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
